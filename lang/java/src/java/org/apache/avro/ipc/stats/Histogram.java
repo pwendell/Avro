cdc0db94af607671529ced9f455b1c2ee4442010
Patrick Wendell <patrick@patrick-laptop.(none)>
2010-06-23 15:57:21 -0700

    Style fix for histogram

--- lang/java/src/java/org/apache/avro/ipc/stats/Histogram.java
+++ lang/java/src/java/org/apache/avro/ipc/stats/Histogram.java
@@ -22,7 +22,6 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeMap;
 
