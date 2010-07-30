11e74821d2a5d8f8c4d8b424a0465a84561b94d8
Patrick Wendell <patrick@patrick-laptop.(none)>
2010-07-30 12:53:52 -0700

    Preliminary add of File Storage

---
+++ lang/java/src/java/org/apache/avro/ipc/trace/FileSpanStorage.java
@@ -0,0 +1,25 @@
+package org.apache.avro.ipc.trace;
+
+import java.util.List;
+
+public class FileSpanStorage implements SpanStorage {
+
+  @Override
+  public void addSpan(Span s) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public List<Span> getAllSpans() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void setMaxSpans(long maxSpans) {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
